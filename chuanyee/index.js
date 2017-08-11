255f879e4512e919301c24214b94187b857e2985
ywpin <wakaka160216@gmail.com>
2017-08-11 16:34:49 +0800

    init

---
+++ chuanyee/index.js
@@ -0,0 +1,16 @@
+$('.seat').on('click',function(){
+	$(this).addClass('booking');
+});
+
+
+$('.book').on('click',function(){
+	$('.booking').addClass('booked');
+	alert('success');
+});
+
+
+
+$('.reset').on('click',function(){
+	location.reload();
+});
+
